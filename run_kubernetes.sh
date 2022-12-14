344e54277b540fc370ddb74acb06d2233b726b4a
Ubuntu <ubuntu@ip-172-31-11-86.ec2.internal>
2022-12-14 10:08:01 +0000

    final

---
+++ run_kubernetes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+dockerpath="lengochung1/flaskapp"
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run flaskapp --image=$dockerpath --port=80 --labels app=flaskapp
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods -o wide
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward flaskapp 8000:80
